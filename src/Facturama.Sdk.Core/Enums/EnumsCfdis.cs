76e4070499cb4a09797ff724780faa190f6c5e0d
Rafael Dorantes <jesus.dorantes.89@gmail.com>
2026-02-17 14:18:06 -0600

    feat(core): Add EnumsCfdis

---
+++ src/Facturama.Sdk.Core/Enums/EnumsCfdis.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.Serialization;
+
+
+namespace Facturama.Sdk.Core.Enums
+{
+    public enum CfdiType
+    {
+        [EnumMember(Value = "I")]
+        Ingreso,
+
+        [EnumMember(Value = "E")]
+        Egreso,
+
+        [EnumMember(Value = "T")]
+        Traslado,
+
+        [EnumMember(Value = "P")]
+        Pago,
+
+        [EnumMember(Value = "N")]
+        Nomina
+    }
+
+    public enum  FileFormat
+    {
+        Xml, Pdf, Html
+    }
+
+    public enum InvoiceType
+    {
+        Issued, Received, Payroll
+    }
+
+    public enum InvoiceStatus
+    {
+        all, Active, Cancelled
+    }
+
+}
--- src/Facturama.Sdk.Core/Enums/FacturamaEnvironment.cs
+++ src/Facturama.Sdk.Core/Enums/FacturamaEnvironment.cs
@@ -1,11 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace src.Facturama.Sdk.Core.Enums
+namespace Facturama.Sdk.Core.Enums
 {
     public enum FacturamaEnvironment
     {
