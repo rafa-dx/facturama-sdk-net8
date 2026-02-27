29efdeb4b2985d5302bcbfac217970b2badafdbd
Rafael Dorantes <jesus.dorantes.89@gmail.com>
2026-02-27 17:08:34 -0600

    Feat(samples): Add examples of branch office and series.

---
+++ samples/Facturama.Sdk.Samples.Console/WebApiExamples/BranchOfficeExample.cs
@@ -0,0 +1,112 @@
+﻿using Facturama.Sdk.Core.Abstractions;
+using Facturama.Sdk.Core.Exceptions;
+using Facturama.Sdk.Core.Models.Common;
+
+
+namespace Facturama.Sdk.Samples.ConsoleApp.WebApiExamples
+{
+    public class BranchOfficeExample : IExample
+    {
+        private readonly IFacturamaClient _facturama;
+
+        public string Name => "3";
+
+        public string Description => "CRUD sucursales";
+
+        public BranchOfficeExample(IFacturamaClient facturama)
+        {
+            _facturama = facturama;
+        }
+
+        public async Task ExecuteAsync()
+        {
+            System.Console.WriteLine("\n--- CRUD sucursales ---");
+            try
+            {
+                await ListBranchOfficesExample();
+                await CreateBranchOfficeExample();
+                await UpdateBranchOfficeExample();
+                await DeleteBranchOfficeExample();
+
+            }
+            catch (FacturamaException ex)
+            {
+                Console.WriteLine($"Error al listar sucursales: {ex.Message}");
+            }
+        }
+
+        public async Task ListBranchOfficesExample()
+        {
+            Console.WriteLine("Listando solo las primeras 100 sucursales...");
+            var branchOffices = await _facturama.BranchOffices.ListAsync();
+            Console.WriteLine($"Total de sucursales: {branchOffices.Count}");
+            foreach (var branchOffice in branchOffices)
+            {
+                Console.WriteLine($"  - [{branchOffice.Id}] {branchOffice.Name}");
+            }
+        }
+
+        public async Task CreateBranchOfficeExample()
+        {
+            Console.Write("Creando una nueva sucursal de prueba...");
+            var newBranchOffice = new Core.Models.Request.BranchOfficeRequest
+            {
+                Name = "El sauce",
+                Description = "Sucursal del sauce, enfocada en la distribución de agua en garrafón",
+                Address = new Address
+                {
+
+                    Street = "Av. del Sauce",
+                   ExteriorNumber = "120",
+                   InteriorNumber = "",
+                   Neighborhood = "Las Flores",
+                   ZipCode ="78116",
+                   Locality = "Localidad",
+                   Municipality = "San Luis Potosi",
+                   State = "San Luis Potosi",
+                   Country = "México"
+                  }
+            };
+            var request = await _facturama.BranchOffices.CreateAsync(newBranchOffice);
+            Console.WriteLine($"Sucursal creada: [{request.Id}] {request.Name} ");
+        }
+
+        public async Task UpdateBranchOfficeExample()
+        {
+            var IdBranchOffice = "GzU8suBbDfiee_O5lvGt-A2"; 
+            var branchOfficeToUpdate = new Core.Models.Request.BranchOfficeRequest
+            {
+                Id = IdBranchOffice,
+                Name = "El sauce",
+                Description = "Sucursal del sauce, enfocada en la distribución de agua en garrafón",
+                Address = new Address
+                {
+
+                    Street = "Av. del Sauce",
+                    ExteriorNumber = "120",
+                    InteriorNumber = "",
+                    Neighborhood = "Las Flores",
+                    ZipCode = "78116",
+                    Locality = "Localidad",
+                    Municipality = "San Luis Potosi",
+                    State = "San Luis Potosi",
+                    Country = "México"
+                }
+            };
+            var response = await _facturama.BranchOffices.UpdateAsync( IdBranchOffice, branchOfficeToUpdate);
+
+         
+            Console.WriteLine($"Sucursal actualizada: [{response.Id}] {response.Name}");
+        }
+
+        public async Task DeleteBranchOfficeExample()
+        {
+            Console.WriteLine("Eliminando la sucursal de prueba...");
+            var idBranchOffice = "FzS_VLdlowXsKANyHqYYkQ2";
+
+            await _facturama.BranchOffices.DeleteAsync(idBranchOffice);
+            Console.WriteLine($"Sucursal eliminada: [{idBranchOffice}] ");
+        }
+    }
+
+    }
---
+++ samples/Facturama.Sdk.Samples.Console/WebApiExamples/SerieExample.cs
@@ -0,0 +1,64 @@
+﻿using Facturama.Sdk.Core.Abstractions;
+
+
+namespace Facturama.Sdk.Samples.ConsoleApp.WebApiExamples
+{
+    public class SerieExample : IExample
+    {
+        private readonly IFacturamaClient _facturama;
+
+        public string Name => "4";
+
+        public string Description => "CRUD series";
+
+        public SerieExample(IFacturamaClient facturama)
+        {
+            _facturama = facturama;
+        }
+
+        public async Task ExecuteAsync()
+        {
+            System.Console.WriteLine("\n--- CRUD series ---");
+            try
+            {
+                //await ListSeriesExample();
+                //await CreateSerieExample();
+                //await DeleteSerieExample();
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine($"Error al listar series: {ex.Message}");
+            }
+
+
+        }
+
+        public async Task ListSeriesExample()
+        {
+            Console.WriteLine("Listando de las series de un lugar de expedición...");
+            string Id = "GkJL10RzjP7G7-c4zxv9uw2";
+
+            var series = await _facturama.Series.ListSerieAsync(Id);
+
+            Console.WriteLine($"Total de series: {series.Count}");
+            foreach (var serie in series)
+            {
+                Console.WriteLine($"  - [{serie.Name}] {serie.Folio} ({serie.Description})");
+            }
+        }
+
+        public async Task CreateSerieExample()
+        {
+            Console.WriteLine("Creando una nueva serie...");
+            string Id = "GkJL10RzjP7G7-c4zxv9uw2";
+            var serie = await _facturama.Series.CreateSerieAsync(Id, new Core.Models.Request.SerieRequest
+            {
+                IdBranchOffice = Id,
+                Name = "A",
+                Description = "Serie A",
+                Folio = 1
+            });
+            Console.WriteLine($"Serie creada: {serie.Name} - {serie.Description}");
+        }
+    }
+}
